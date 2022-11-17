8ca16dc63de3884c184b2d5a4466d5eb5ecd506d
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-17 18:14:15 +0700

    make class node and declare var

--- DoubleLinkedList/Program.cs
+++ DoubleLinkedList/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace DoubleLinkedList
 {
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+        * It consoscts of the information part and links to
+        * its succeeding and preceeding nodes
+        * in terms od next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the suceeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
     internal class Program
     {
         static void Main(string[] args)
